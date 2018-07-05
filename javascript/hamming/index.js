79b5fa4b7bf8a85bc37494b39761813319e8a992
James Robbins <highplainsjames@gmail.com>
2018-07-05 11:27:21 -0500

    Added Initial JS Implementation for Calculating Hamming Value

--- javascript/hamming/index.js
+++ javascript/hamming/index.js
@@ -3,5 +3,46 @@
 // your code meets all of the conditions.
 // You may use ES6 or ES5 to solve.
 
+"use strict"
+
+class Hamming {
+  validateAndFormatStrand(strand) {
+    if (typeof strand !== 'string') {
+      throw 'Invalid Strand Type';
+    }
+
+    return strand.toUpperCase();
+  }
+  validateStrandLengths(firstStrand, secondStrand) {
+    if (firstStrand.length !== secondStrand.length) {
+      throw 'DNA strands must be of equal length.';
+    }
+  }
+  compute(firstStrand, secondStrand) {
+    firstStrand = this.validateAndFormatStrand(firstStrand);
+    secondStrand = this.validateAndFormatStrand(secondStrand);
+
+    this.validateStrandLengths(firstStrand, secondStrand);
+
+    // quick equality check
+    if (firstStrand === secondStrand) {
+      return 0;
+    }
+    else {
+      return this.calculateHammingValue(firstStrand, secondStrand);
+    }
+  }
+  calculateHammingValue(firstStrand, secondStrand) {
+    let hammingValue = 0;
+
+    for(let i=0; i < firstStrand.length; i++) {
+      if (firstStrand[i] !== secondStrand[i])  {
+        hammingValue++;
+      }
+    }
+
+    return hammingValue;
+  }
+}
 
 module.exports = Hamming;
